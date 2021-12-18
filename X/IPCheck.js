460af191189522e1f2bd5b38326f4bf2115fce28
未名。 <unknowntokyo@users.noreply.github.com>
2021-12-18 12:14:39 +0800

    Update

--- X/IPCheck.js
+++ X/IPCheck.js
@@ -17,8 +17,8 @@ var flags = new Map([[ "AC" , "🇦🇨" ] ,["AE","🇦🇪"], [ "AF" , "🇦�
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = flags.get(obj['countryCode']) + ' '+ obj['country'];
-var subtitle = ISP_ValidCheck(obj['org']) + obj['query'] + obj['asn_org'];
+var subtitle = obj['query'];
 var ip = obj['query'];
-var description = obj['org'] + '\n' + obj['query'];
+var description = obj['query'];
 
 $done({title, subtitle, ip, description});
