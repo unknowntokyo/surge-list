022669827105b3e360fe4192fd2625a541626e0e
未名。 <unknowntokyo@users.noreply.github.com>
2021-12-22 23:00:21 +0800

    Update

--- Surge/NET_info.js
+++ Surge/NET_info.js
@@ -1,8 +1,6 @@
 setTimeout(function () {
 ;(async () => {
 
-
-
 let params = getParams($argument)
 //获取根节点名
 let proxy = await httpAPI("/v1/policy_groups");
@@ -12,9 +10,6 @@ for (var key in proxy){
     }
 let group = params.group
 let rootName = (await httpAPI("/v1/policy_groups/select?group_name="+encodeURIComponent(group)+"")).policy;
-while(allGroup.includes(rootName)==true){
-	rootName = (await httpAPI("/v1/policy_groups/select?group_name="+encodeURIComponent(rootName)+"")).policy;
-}
 
 $httpClient.get('http://ip-api.com/json/?lang=en', function (error, response, data) {
     const jsonData = JSON.parse(data);
@@ -48,4 +43,4 @@ function getParams(param) {
       .map(([k, v]) => [k, decodeURIComponent(v)])
   );
 }
-}, 6000)
+}, 200)
