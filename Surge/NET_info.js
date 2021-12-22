5ec7635d1cdc5f8f0f84d78b6ef11ac9be9d74b1
未名。 <unknowntokyo@users.noreply.github.com>
2021-12-22 21:36:35 +0800

    Update

--- Surge/NET_info.js
+++ Surge/NET_info.js
@@ -1,33 +1,37 @@
 ;(async () => {
+
+
+
 let params = getParams($argument)
 //获取根节点名
-let proxy = await httpAPI(”/v1/policy_groups“);
+let proxy = await httpAPI("/v1/policy_groups");
 let allGroup = [];
 for (var key in proxy){
    allGroup.push(key)
     }
 let group = params.group
-let rootName = (await httpAPI(”/v1/policy_groups/select?group_name=“+encodeURIComponent(group)+”“)).policy;
+let rootName = (await httpAPI("/v1/policy_groups/select?group_name="+encodeURIComponent(group)+"")).policy;
 while(allGroup.includes(rootName)==true){
-	rootName = (await httpAPI(”/v1/policy_groups/select?group_name=“+encodeURIComponent(rootName)+”“)).policy;
+	rootName = (await httpAPI("/v1/policy_groups/select?group_name="+encodeURIComponent(rootName)+"")).policy;
 }
 
-$httpClient.get(’http://ip-api.com/json/?lang=en‘, function (error, response, data) {
+$httpClient.get('http://ip-api.com/json/?lang=en', function (error, response, data) {
     const jsonData = JSON.parse(data);
     $done({
       title:rootName,
       content:
-		`地区: ${jsonData.city}\n`+
-      `运营商: ${jsonData.isp}\n` +
-		`数据中心: ${jsonData.org}`,
+		`Area: ${jsonData.city}\n`+
+      `Carrier : ${jsonData.isp}\n` +
+		`Data center: ${jsonData.org}`,
       icon: params.icon,
-		  ”icon-color“:params.color
+		  "icon-color":params.color
     });
   });
 
 })();
 
-function httpAPI(path = ”“, method = ”GET“, body = null) {
+
+function httpAPI(path = "", method = "GET", body = null) {
     return new Promise((resolve) => {
         $httpAPI(method, path, body, (result) => {
             resolve(result);
@@ -38,8 +42,9 @@ function httpAPI(path = ”“, method = ”GET“, body = null) {
 function getParams(param) {
   return Object.fromEntries(
     $argument
-      .split(”&“)
-      .map((item) => item.split(”=“))
+      .split("&")
+      .map((item) => item.split("="))
       .map(([k, v]) => [k, decodeURIComponent(v)])
   );
 }
+
