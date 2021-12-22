b46c72f3db7ea56119c0d8466dd1c1cedc7eecde
未名。 <unknowntokyo@users.noreply.github.com>
2021-12-22 23:10:00 +0800

    Update

--- Surge/NET_info.js
+++ Surge/NET_info.js
@@ -11,6 +11,8 @@ for (var key in proxy){
 let group = params.group
 let rootName = (await httpAPI("/v1/policy_groups/select?group_name="+encodeURIComponent(group)+"")).policy;
 
+	rootName = (await httpAPI("/v1/policy_groups/select?group_name="+encodeURIComponent(rootName)+"")).policy;
+
 $httpClient.get('http://ip-api.com/json/?lang=en', function (error, response, data) {
     const jsonData = JSON.parse(data);
     $done({
