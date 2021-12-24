c3ee7b48782afc19439e4296a42556fa5b143eed
未名。 <unknowntokyo@users.noreply.github.com>
2021-12-25 00:07:40 +0800

    Update

--- Surge/COVID-19.js
+++ Surge/COVID-19.js
@@ -1,6 +1,7 @@
 var list = ["中国"];
 const url = "https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5";
 var ala="";
+var ala1="";
 function num(location, result) {
   var loc = location;
   var resu = result;
@@ -10,10 +11,12 @@ function num(location, result) {
   let loc_now_res = loc_now.exec(resu);
   if (loc_new_res) {
     //console.log("已获取" + loc + "的信息");
-    ala = ala +loc +"新增" +loc_new_res[1].padStart(5,"\u0020")+"例子，现存"+loc_now_res[1].padStart(5,"\u0020")+"例";
+    ala = ala +loc +"：新增" +loc_new_res[1].padStart(5,"\u0020")+"例，现存"+loc_now_res[1].padStart(5,"\u0020")+"例";
+    ala1=ala.replace(/\n$/, "");
   } else {
     //console.log("获取" + loc + "的信息失败");
-    ala = ala + loc + "   :   查无数据";
+    ala = ala + loc + "查无数据";
+    ala1=ala.replace(/\n$/, "");
   }
 };
 $httpClient.get(url, function(error, response, data){
@@ -22,10 +25,10 @@ $httpClient.get(url, function(error, response, data){
     num(list[i], res);
     if (i == list.length - 1) {
      $done({
-       title: "COVID-2019",
+       title: "COVID-19",
        icon:"filemenu.and.cursorarrow",
        "icon-color":"#0089A7",
-       content: ala.replace(/\n$/, "")
+       content: ala1.replace(/\s/g, "")
      });
     }
   }
