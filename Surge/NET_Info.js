05e78965e6d05f10dd8a3310f83853d36e28b0f4
未名。 <unknowntokyo@users.noreply.github.com>
2021-12-25 01:46:57 +0800

    Update

--- Surge/NET_Info.js
+++ Surge/NET_Info.js
@@ -8,7 +8,7 @@ let rootName = (await httpAPI("/v1/policy_groups/select?group_name="+encodeURICo
 $httpClient.get('http://ip-api.com/json/?lang=en', function (error, response, data) {
     const jsonData = JSON.parse(data);
     $done({
-      title:节点信息：rootName,
+      title:"节点信息："+rootName,
       content:
 		`IP地址：${jsonData.query}\n`+
       `运营商：${jsonData.isp}\n` +
