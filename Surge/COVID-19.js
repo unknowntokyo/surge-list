531fd03f332b5adee8b86f4d0fc8dc026675815d
未名。 <unknowntokyo@users.noreply.github.com>
2021-12-25 00:17:45 +0800

    Update

--- Surge/COVID-19.js
+++ Surge/COVID-19.js
@@ -1,4 +1,4 @@
-var list = ["湖北","武汉","十堰"];
+var list = ["中国","江苏","安徽"];
 const url = "https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5";
 var ala="";
 var ala1="";
@@ -12,11 +12,11 @@ function num(location, result) {
   if (loc_new_res) {
     //console.log("已获取" + loc + "的信息");
     ala = ala +loc +"       " +loc_new_res[1].padStart(5,"\u0020")+"  "+loc_now_res[1].padStart(5,"\u0020")+ "\n";
-    ala1=ala.replace(/\n$/, "");
+    ala1=ala.replace(/\s/g, "");
   } else {
     //console.log("获取" + loc + "的信息失败");
     ala = ala + loc + "   :   查无数据\n";
-    ala1=ala.replace(/\n$/, "");
+    ala1=ala.replace(/\s/g, "");
   }
 };
 $httpClient.get(url, function(error, response, data){
@@ -28,7 +28,7 @@ $httpClient.get(url, function(error, response, data){
        title: "COVID-19：   新增  |  现存",
        icon:"filemenu.and.cursorarrow",
        "icon-color":"#0089A7",
-       content: ala1.replace(/\s/g, "")
+       content: ala1.replace(/\n$/, "")
      });
     }
   }
