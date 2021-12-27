866c60e77c171a34a98afcfa575b5207d9e2cae6
未名。 <unknowntokyo@users.noreply.github.com>
2021-12-27 19:37:52 +0800

    Update

--- X/IPCheck.js
+++ X/IPCheck.js
@@ -3,6 +3,22 @@ var obj = JSON.parse(body);
 var title = '' + ' '+ obj['country'];
 var subtitle = obj['org'];
 var ip = obj['query'];
-var description = obj['query'];
-
-$done({title, subtitle, ip, description});
+var carrier = obj['isp'];
+var datacenter = obj['org'];
+switch (datacenter){
+    case "":
+      if (carrier != "") {
+      var description = ip + '\n' + carrier;
+      } else {
+      var description = ip;
+      }
+      break
+    default:
+      if (carrier != "") {
+      var description = ip + '\n' + carrier + '\n' + datacenter;
+      } else {
+      var description = ip + '\n' + datacenter;
+      }
+      break
+  }
+  $done({title, subtitle, ip, description});
