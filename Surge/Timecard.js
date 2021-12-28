a90064703d96b0e1f7ee7fa894f8dc4fe9f70bfc
未名。 <unknowntokyo@users.noreply.github.com>
2021-12-28 19:05:16 +0800

    Update

--- Surge/Timecard.js
+++ Surge/Timecard.js
@@ -61,8 +61,8 @@ function today(day) {
 
 $done({
 title:"节假日倒计时",
-icon:"logo.playstation",
-'icon-color': "#000000",
+icon:"hourglass",
+'icon-color': "#3394E6",
 content:
 tlist[nowlist][0]+"："+today(tnumcount(nowlist))+"天"+tlist[nowlist][2]+"\n"+tlist[Number(nowlist) + Number(1)][0] +"："+ tnumcount(Number(nowlist) + Number  (1))+ "天"+tlist[Number(nowlist) + Number(1)][2]+"\n"+tlist[Number(nowlist) + Number(2)][0]+"："+tnumcount(Number(nowlist) + Number(2))+"天"+tlist[Number(nowlist) + Number(2)][2]
 })
