6ee49ac7dc363dbcac01b3c4138a8901bdad8025
未名。 <1363691502@qq.com>
2026-03-19 22:33:16 +0800

    Update alpn.js

--- X/alpn.js
+++ X/alpn.js
@@ -2,7 +2,7 @@ function operator(proxies, targetPlatform) {
     const alpn = "02683208687474702f312e31";
     proxies.forEach(proxy => {
         if (targetPlatform === "QX") {
-            proxy.tfo = `${proxy.tfo || false}, tls-alpn=${alpn}`;
+            proxy.tfo = `${proxy.tfo}, tls-alpn=${alpn}`;
         }
     });
     return proxies;
