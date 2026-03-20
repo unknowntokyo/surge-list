905e6f5338ceaa74f4b854016a7512f45533092a
未名。 <1363691502@qq.com>
2026-03-20 22:26:54 +0800

    Update streaming-ui-check.js

--- X/streaming-ui-check.js
+++ X/streaming-ui-check.js
@@ -113,7 +113,7 @@ function testNf(filmId) {
     let option = {
       url: BASE_URL + filmId,
       opts: opts,
-      timeout: 5200,
+      timeout: 2800,
       headers: {
         'User-Agent':
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
