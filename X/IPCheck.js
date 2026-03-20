e7855d8942853a20404a782c156e6bb6f7347072
未名。 <1363691502@qq.com>
2026-03-20 20:16:09 +0800

    Update IPCheck.js

--- X/IPCheck.js
+++ X/IPCheck.js
@@ -7,10 +7,10 @@ var city = obj['city_name'] || 'unknown';
 var asn = 'AS' + obj['asn'];
 var datacenter = obj['as_desc'];
 datacenter = datacenter.replace(/,/g, ' ').replace(/\./g, '').replace(/\([^\)]*\)/g, "");
-var subtitle = asn + ' ' + datacenter;
-if (subtitle.length >= 50) {
-    subtitle = subtitle.replace(/(?: Limited| LLC| LTD| GmbH|, Inc|, Inc\.| Corporation| Co\.,\s*Ltd\.| PTE LTD)/ig, "");
+if (datacenter.length >= 35) {
+    datacenter = datacenter.replace(/(?: Limited| LLC| LTD| GmbH|, Inc|, Inc\.| Corporation| Co\.,\s*Ltd\.| PTE LTD)/ig, "");
 }
+var subtitle = asn + ' ' + datacenter;
 
 var CountryCodeMap = new Map([
     ["HK", "HKG"],
