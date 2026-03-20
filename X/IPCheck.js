37a8d3f014c6ac7388a0f3caed6c918696e0a704
未名。 <1363691502@qq.com>
2026-03-20 20:01:18 +0800

    Update IPCheck.js

--- X/IPCheck.js
+++ X/IPCheck.js
@@ -6,7 +6,7 @@ var ip = obj['ip'];
 var city = obj['city_name'] || 'unknown';
 var asn = 'AS' + obj['asn'];
 var datacenter = obj['as_desc'];
-var subtitle = String(asn + ' ' + datacenter).replace(/,/g, ' ').replace(/\./g, '').replace(/\([^\)]*\)/g, "");
+var subtitle = (asn + ' ' + datacenter).replace(/,/g, ' ').replace(/\./g, '').replace(/\([^\)]*\)/g, "");
 if (subtitle.length >= 50) {
     subtitle = subtitle.replace(/(?: Limited| LLC| LTD| GmbH|, Inc|, Inc\.| Corporation| Co\.,\s*Ltd\.| PTE LTD)/ig, "");
 }
