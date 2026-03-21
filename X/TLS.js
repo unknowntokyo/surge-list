5aa419edee3a1fbafddd7ce70ed6a31016f9b4e0
未名。 <1363691502@qq.com>
2026-03-21 12:45:54 +0800

    Update TLS.js

--- X/TLS.js
+++ X/TLS.js
@@ -2,7 +2,7 @@ function operator(proxies, targetPlatform) {
     const alpn = "02683208687474702f312e31";
     proxies.forEach(proxy => {
         if (targetPlatform === "QX") {
-            proxy.tfo = `${proxy.tfo}, tls-alpn=${alpn}, tls-no-session-ticket=true`;
+            proxy.tfo = `${proxy.tfo}, tls13=true, tls-alpn=${alpn}, tls-no-session-ticket=true`;
         }
     });
     return proxies;
